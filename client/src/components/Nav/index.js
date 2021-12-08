7a829071987334671435386ff73f73626376ffef
ca2los <{ID}+{username}@users.noreply.github.com>
2021-12-07 20:16:33 -0600

    New components

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -48,10 +48,10 @@
                 );
             }
         }
-        
+
         const navbar = document.querySelector('.header');
         window.onscroll = () => {
-            if (window.scrollY > 300) {
+            if (window.scrollY > 100) {
                 navbar.classList.add('nav-active');
             } else {
                 navbar.classList.remove('nav-active');
